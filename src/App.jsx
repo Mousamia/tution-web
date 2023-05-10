9df5493a1a141c79a707272f62aafd0e7ef0bd40
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-11 03:38:57 +0600

    dataadded into server

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { stringify } from 'postcss';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const users = { name, email };
+
+    console.log(users);
+
+    fetch('http://localhost:5000/users', {
+      method: 'post',
+      header: {
+        'content-type': 'application/json',
+      },
+
+      body: JSON.stringify(users)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+
+
+  }
+
+
 
   return (
     <div className="App">
-      <h1>Samiaaaaaa</h1>
+
+      {
+        users.map(user => <h2> Name: {user.name} : {user.email} </h2>)
+      }
+
+      <form className='flex justify-center' onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <input type="email" name="email" id="" />
+        <input type="submit" value="Submit" />
+      </form>
+
     </div>
   )
 }
---
+++ src/components/serverData.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const serverData = () => {
+    return (
+        <div>
+            data here
+        </div>
+    );
+};
+
+export default serverData;
